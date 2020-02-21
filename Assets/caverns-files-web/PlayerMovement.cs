8347243c5ad0cd67dbbb7003ef87ae5e9ebfcc45
SheltonEllis <60905093+SheltonEllis@users.noreply.github.com>
2020-02-20 21:12:18 -0600

    WalkAnimation/Lever

--- Assets/caverns-files-web/PlayerMovement.cs
+++ Assets/caverns-files-web/PlayerMovement.cs
@@ -7,6 +7,7 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public CharacterController2D controller;
+    public Animator animator;
 
     public float runSpeed = 40f;
 
@@ -19,6 +20,8 @@ public class PlayerMovement : MonoBehaviour
     {
 
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
 
         if (Input.GetButtonDown("Jump"))
         {
