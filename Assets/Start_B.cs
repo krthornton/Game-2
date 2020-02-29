20ba191996bd2bae3db583a98b42d7a6ce9e0c29
bham333 <60905082+bham333@users.noreply.github.com>
2020-02-28 20:59:57 -0600

    everything
    hopefullyeverything

--- Assets/Start_B.cs
+++ Assets/Start_B.cs
@@ -2,20 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class B_Main_script : MonoBehaviour
+
+public class Start_B : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
-        
+
     }
     public void Play_Game()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+    public void Mnu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void info()
+    {
+        SceneManager.LoadScene("Information");
+    }
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
 }
